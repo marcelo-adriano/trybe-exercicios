492a5c3bcaff36b06511a46405c1dba98b9531f5
Marcelo Adriano <marcelo.gomes.adriano@gmail.com>
2021-07-29 15:19:51 -0300

    Exercicios do conteúdo 'for'

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-progamacao/dia-2-javascript-array-e-loop-for/exercicios-de-fixacao/exercicio04.js
@@ -0,0 +1,5 @@
+let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
+
+for (let index = 0; index < groceryList.length; index += 1) {
+    console.log((index+1)+'º item da lista: '+groceryList[index]);
+}
