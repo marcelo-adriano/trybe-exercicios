732ba3e21ae5ffc71c7c072a405c4bc7c8011822
Marcelo Adriano <marcelo.gomes.adriano@gmail.com>
2021-07-31 22:25:58 -0300

    Alteração no exercício 4, das atividades pós aula, do dia 3 do bloco 4.

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-progamacao/dia-3-javascript-logica-de-programacao-e-algoritmos/exercicios-pos-aula/exercicio01.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-progamacao/dia-3-javascript-logica-de-programacao-e-algoritmos/exercicios-pos-aula/exercicio01.js
@@ -1,5 +1,5 @@
 //Para o primeiro exercício de hoje, faça um programa que, dado um valor n qualquer, seja n 
-//> 1 , imprima na tela um quadrado feito de asteriscos de lado de tamanho n . Por exemplo: 
+//> 1 , imprima na tela um quadrado feito de asteriscos de lado de tamanho n.
 
 function inteiroAleatorio() {
     return Math.round(Math.random()*10);
--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-progamacao/dia-3-javascript-logica-de-programacao-e-algoritmos/exercicios-pos-aula/exercicio04.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-progamacao/dia-3-javascript-logica-de-programacao-e-algoritmos/exercicios-pos-aula/exercicio04.js
@@ -4,48 +4,74 @@ function inteiroAleatorio() {
     return Math.round(Math.random()*(15-3)+3);
 }
 
-let n = inteiroAleatorio();
-let arrayDeAsterisco;
+let base = inteiroAleatorio();
+/*
+Revisando o Conteúdo com Ítalo Sérgio, da turma 15 - tribo A, observamos a seguinte relação
 
-console.log(' - - - '+n+' X '+Math.round(n/2)+' - - - ')
-if (n%2 !== 0) {
+Base |  Altura
+ 1   |   1
+ 2   |   1
+ 3   |   2
+ 4   |   2
+ 5   |   3
+ 6   |   3
+ 7   |   4
+ 8   |   4
+ 9   |   5
+10   |   5
 
-    for (let index = 0; index < n/2; index += 1) {
-        arrayDeAsterisco = '';
-        let numeroAsterisco = 1+(index*2);
-        let numeroEspacos = n - numeroAsterisco;
-    
-        for (let indexDois = 0; indexDois < numeroEspacos/2; indexDois += 1) {
-            arrayDeAsterisco += ' ';
-        }
-        for (let indexTres = 1; indexTres <= numeroAsterisco; indexTres += 1) {
-            arrayDeAsterisco += '*';
-        }
-        for (let indexDois = 0; indexDois < numeroEspacos/2; indexDois += 1) {
-            arrayDeAsterisco += ' ';
-        }
-        
-        console.log(arrayDeAsterisco);
-    }
+vimos que, se base for par, a altura é base/2
+
+        Base |  Altura
+        2    |   1
+        4    |   2
+        6    |   3
+        8    |   4
+        10   |   5
+        x    |   x/2
+        se a base for n, x=n, então altura=n/2
+
+mas se a base for impar, a altura é (base+1)/2
+        Base |  Altura
+        1    |   1
+        3    |   2
+        5    |   3
+        7    |   4
+        9    |   5
+       1+2x  |  1+x
+       se a base for n, logo x=(n-1)/2,então altura=((n-1)/2)+1
+*/
+let altura;
+let linhaConstruida = '';
+let asteriscosIniciais = 0;
+
+if (base%2 !== 0) {
+    altura = ((base-1)/2)+1;
+    console.log(' - - - '+base+' X '+altura+' - - - ');
+    asteriscosIniciais = 1;
     
 } else {
-    
-    for (let index = 0; index < n/2; index += 1) {
-        arrayDeAsterisco = '';
-        let numeroAsterisco = 2+(index*2);
-        let numeroEspacos = n - numeroAsterisco;
+    altura = base/2;
+    console.log(' - - '+base+' X '+altura+' - - ');
+    asteriscosIniciais = 2;
+}
 
-        for (let indexDois = 0; indexDois < numeroEspacos/2; indexDois += 1) {
-            arrayDeAsterisco += ' ';
-        }
-        for (let indexTres = 1; indexTres <= numeroAsterisco; indexTres += 1) {
-            arrayDeAsterisco += '*';
-        }
-        for (let indexDois = 0; indexDois < numeroEspacos/2; indexDois += 1) {
-            arrayDeAsterisco += ' ';
-        }
-        
-        console.log(arrayDeAsterisco);
+for (let index = 0; index < altura; index += 1) {
+    linhaConstruida = '';
+    let direita = '';
+    let esquerda = '';
+    let asterisco = '';
+
+    let numeroAsterisco = asteriscosIniciais+(index*2);
+    let numeroEspacos = base - numeroAsterisco;
+
+    for (let indexDois = 0; indexDois < numeroEspacos/2; indexDois += 1) {
+        direita += ' ';
+        esquerda += ' ';
     }
-
+    for (let indexTres = 1; indexTres <= numeroAsterisco; indexTres += 1) {
+        asterisco += '*';
+    }
+    linhaConstruida = esquerda + asterisco + direita
+    console.log(linhaConstruida);
 }
