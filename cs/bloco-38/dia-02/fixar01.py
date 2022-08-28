1541ad641708d6822f740855831ea344390e4ec0
Marcelo Adriano <marcelo.gomes.adriano@gmail.com>
2022-08-28 05:16:46 -0300

    fim do curso

---
+++ cs/bloco-38/dia-02/fixar01.py
@@ -0,0 +1,42 @@
+class Conjunto:
+    def __init__(self):
+        self._data = [False for i in range(1000)]
+        self.last_element = 0
+
+    def __str__(self):
+        string = "{"
+
+        for index, is_in_set in enumerate(self._data):
+            if is_in_set:
+                string += str(index)
+                if index < self.last_element:
+                    string += ", "
+
+        string += "}"
+        return string
+
+    def add(self, item):
+        if not self._data[item]:
+            self._data[item] = True
+        if item > self.last_element:
+            self.last_element = item
+
+
+if __name__ == "__main__":
+    conj = Conjunto()
+    for i in [0, 10, 100, 1000]:
+        conj.add(i)
+    print(conj)
+
+    conj2 = Conjunto()
+    for i in [1, 2, 3]:
+        conj2.add(i)
+    print(conj2)
+
+    conj3 = Conjunto()
+    for i in [7, 2, 10]:
+        conj3.add(i)
+    print(conj3)
+
+    conj4 = Conjunto()
+    print(conj4)
